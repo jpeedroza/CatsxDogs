4a10d74162ce5e1bd22699a005b2fd3c80f7ce66
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-05-26 20:43:00 -0300

    Estilizacão da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [cats, setCats] = useState(0);
+  const [dogs, setDogs] = useState(0);
+
+  useEffect(() => {
+    console.log(`Dogs:${dogs} Cats:${cats}`);
+  }, [dogs, cats])
+
   return (
     <div className="App">
-      <div>
-        <div>
-          <h2>Cats</h2>
-          <p>{100}%</p>
-        </div>
-        <span>X</span>
-        <div>
-          <h2>Dogs</h2>
-          <p>{100}%</p>
-        </div>
-      </div>
+      <section onClick={() => setCats(cats + 1)}>
+        <h2>Cats</h2>
+        <p>{100}%</p>
+      </section>
+      <span>X</span>
+      <section onClick={() => setDogs(dogs + 1)}>
+        <h2>Dogs</h2>
+        <p>{100}%</p>
+      </section>
     </div>
   );
 }
