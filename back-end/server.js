c55873213c91fcc5b0b5cfa758ddbebcb4965ecf
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-05-27 21:58:46 -0300

    Criacão das rotas para o DB

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,52 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
-app.use(express.json());
+const conn = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "my-secret-pw",
+  database: "teste",
+});
 
+app.use(express.json());
 app.get("/", (req, res) => {
+  conn.connect();
+  conn.query(`CREATE TABLE IF NOT EXISTS votacao(
+                type VARCHAR(20),
+                votes INT
+              )
+            `);
+  conn.query(`INSERT INTO votacao VALUES ('dogs', 0)`);
+  conn.query(`INSERT INTO votacao VALUES ('cats', 0)`);
+  conn.end();
   res.json({
-    answer: "Worked",
+    res: "ok",
   });
 });
 
-app.listen("3001", () => {
-  console.log("Running");
+app.get("/dogs", async (req, res) => {
+  conn.query(`UPDATE votacao SET votes=votes+1 WHERE type='dogs'`);
+  res.json({
+    res: "ok",
+  });
+});
+
+app.get("/cats", async (req, res) => {
+  conn.query(`UPDATE votacao SET votes=votes+1 WHERE type='cats'`);
+  res.json({
+    res: "ok",
+  });
+});
+
+app.get("/cleanup", async (req, res) => {
+  conn.query(`UPDATE votacao SET votes=0 WHERE type='dogs' OR type='cats'`);
+  res.json({
+    res: "ok",
+  });
+});
+
+app.listen(3001, () => {
+  console.log("conectado");
 });
