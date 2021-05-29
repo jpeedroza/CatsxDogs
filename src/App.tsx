2e530d928de62c75a23754a2646f077ab8c8d376
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-05-28 23:10:16 -0300

    Criacão do envio dos votos ao backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
   const [cats, setCats] = useState(0);
   const [dogs, setDogs] = useState(0);
-
+  const computeValues = (type: string) => {
+    axios.post('/vote', {
+      vote: type
+    })
+  }
   useEffect(() => {
     console.log(`Dogs:${dogs} Cats:${cats}`);
   }, [dogs, cats])
 
   return (
     <div className="App">
-      <section onClick={() => setCats(cats + 1)}>
+      <section onClick={() => {
+        setCats(cats + 1);
+        computeValues("cats")
+      }
+      }>
         <h2>Cats</h2>
         <p>{
           cats === 0 && dogs === 0
@@ -21,7 +30,11 @@ const App: React.FC = () => {
         %</p>
       </section>
       <span>X</span>
-      <section onClick={() => setDogs(dogs + 1)}>
+      <section onClick={() => {
+        setDogs(dogs + 1);
+        computeValues("dogs")
+      }
+      }>
         <h2>Dogs</h2>
         <p>{
           dogs === 0 && cats === 0
